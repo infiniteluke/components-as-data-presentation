3756b2f78e91ab67a86c150174e8f4140358ae98
Luke Herrington <lherrington100@gmail.com>
2019-08-01 21:09:05 -0700

    add fallbacks for iframes

--- components/Iframe.js
+++ components/Iframe.js
@@ -3,31 +3,49 @@ import React from 'react';
 export default (props) => {
   const [loading, setLoading] = React.useState(true);
   const [show, setShow] = React.useState(false);
-  const onLoad = (e) => console.log(e.target) || setLoading(false);
+  const [showFallback, setShowFallback] = React.useState(false);
+  const onLoad = () => setLoading(false);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      if (loading && !show) {
+        setShowFallback(true);
+      }
+    }, 5000);
+    return () => {
+      clearTimeout(timer)
+    };
+  }, [loading, show])
 
   React.useEffect(() => {
     const timer = setTimeout(() => {
       if (!loading) {
         setShow(true);
       }
-    }, 500)
+    }, 500);
     return () => {
       clearTimeout(timer)
     };
   }, [loading])
 
-  let message = loading ? <h1>loading an<br/> iframe lol 🤞🏻</h1> : null;
+  let content = null;
+  if (loading) {
+    content = <h1>loading an<br/> iframe lol 🤞🏻</h1>;
+  }
   if (!loading && !show) {
-    message = <h1>omg it worked 🎉</h1>
+    content = <h1>omg it worked 🎉</h1>;
+  }
+  if (showFallback) {
+    content = props.fallback || <pre>No fallback provided. You're out of luck</pre>;
   }
 
   return (
     <React.Fragment>
-      {message}
+      {content}
       <iframe
         onLoad={onLoad}
         style={{
-          display: !show ? 'none' : 'block',
+          display: !show || showFallback ? 'none' : 'block',
           margin: '2%',
           width: '96%',
           height: '96%',
